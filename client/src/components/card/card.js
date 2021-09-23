a07c147045b8f7512946e9b03c4e21d826a24de0
Rahul Sharma <rahulsharma@Rahuls-Air-276.lan>
2021-09-23 18:41:30 -0400

    FEAT: Highcharts added

--- client/src/components/card/card.js
+++ client/src/components/card/card.js
@@ -1,5 +1,74 @@
 import React from "react";
+import Highcharts from "highcharts";
+import HighchartsReact from "highcharts-react-official";
+
 import "./card.css";
+
+const options = {
+  chart: {
+    plotBackgroundColor: null,
+    plotBorderWidth: null,
+    plotShadow: false,
+    type: "pie",
+    marginTop: 0,
+    marginBottom: 0,
+    width: 300,
+    height: 300,
+    backgroundColor: "transparent",
+  },
+  title: {
+    text: "",
+  },
+  tooltip: {
+    pointFormat: "{series.name}: <b>{point.percentage:.1f}%</b>",
+  },
+  accessibility: {
+    point: {
+      valueSuffix: "%",
+    },
+  },
+  plotOptions: {
+    pie: {
+      allowPointSelect: true,
+      cursor: "pointer",
+      dataLabels: {
+        enabled: true,
+        format: "{point.percentage:.1f} %",
+      },
+    },
+  },
+  colors: Highcharts.map(Highcharts.getOptions().colors, function (color) {
+    return {
+      radialGradient: {
+        cx: 0.5,
+        cy: 0.3,
+        r: 0.7,
+      },
+      stops: [
+        [0, color],
+        [1, Highcharts.color(color).brighten(-0.3).get("rgb")], // darken
+      ],
+    };
+  }),
+  series: [
+    {
+      name: "MatchStats",
+      colorByPoint: true,
+      data: [
+        {
+          name: "Chrome",
+          y: 61.41,
+          sliced: true,
+          selected: true,
+        },
+        {
+          name: "Internet Explorer",
+          y: 100 - 61.41,
+        },
+      ],
+    },
+  ],
+};
 
 const Card = ({ team }) => {
   return (
@@ -9,9 +78,7 @@ const Card = ({ team }) => {
           <img src={team.bg} />
         </div>
         <div className="flip-card-back">
-          <h1>John Doe</h1>
-          <p>Architect & Engineer</p>
-          <p>We love that guy</p>
+          <HighchartsReact highcharts={Highcharts} options={options} />
         </div>
       </div>
     </div>
