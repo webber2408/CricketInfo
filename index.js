0a5447633ce4dc493d4f88bb860abb64f145a747
Aruvansh Nigam <aruvanshnigam@Aruvanshs-Air.lan>
2021-09-23 01:28:27 -0400

    Creating docker files for backend and database

--- index.js
+++ index.js
@@ -1,9 +1,8 @@
-
 const mongoose = require("mongoose");
 const path = require("path");
-const http = require('http');
+const http = require("http");
 // const { v4: uuidv4 } = require('uuid');
-const fastify = require("fastify")({ logger:true });
+const fastify = require("fastify")({ logger: true });
 const SERVER_PORT = 5000;
 
 //CORS
@@ -13,20 +12,24 @@ const SERVER_PORT = 5000;
 // });
 
 //Routes
-const routes = require('./routes/route.js');
+const routes = require("./routes/route.js");
 
 Object.values(routes).forEach((item) => {
-    item.forEach((route) => {
-        fastify.route(route);
-    });
+  item.forEach((route) => {
+    fastify.route(route);
+  });
 });
 
-fastify.get('/', (req,res) => {
-    res.send("Cricket Information Server Started");
+fastify.get("/", (req, res) => {
+  res.send("Cricket Information Server Started");
 });
 
 //MongoDB
-mongoose.connect('mongodb://localhost:27018/cricketInfo', {useNewUrlParser:true, useUnifiedTopology:true})
+mongoose
+  .connect("mongodb://mongo:27018/cricketInfo", {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
   .then(() => {
     console.log("MongoDB Connected");
   })
@@ -35,23 +38,23 @@ mongoose.connect('mongodb://localhost:27018/cricketInfo', {useNewUrlParser:true,
     console.log(err);
   });
 
-
 //Server
 const start = async () => {
-  try{
-    fastify.listen(SERVER_PORT, '0.0.0.0')
-      .then(address => {
+  try {
+    fastify
+      .listen(SERVER_PORT, "0.0.0.0")
+      .then((address) => {
         console.log(`Server started at ${address}`);
       })
-      .catch(err => {
+      .catch((err) => {
         console.log("Error starting server: " + err);
         process.exit(1);
-      })
-  }catch{
+      });
+  } catch {
     fastify.log.error("Error");
     fastify.log.error(err);
     process.exit(1);
   }
-}
+};
 
 start();
