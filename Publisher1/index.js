c394f71d666852760d248d78b6e6431438400917
Aruvansh Nigam <aruvanshnigam@Aruvanshs-Air.lan>
2021-10-22 16:10:45 -0400

    Creating messaging events of publisher 1

--- Publisher1/index.js
+++ Publisher1/index.js
@@ -1,22 +1,28 @@
 const axios = require("axios");
 const CONFIG = require("./config.json");
-var leagueByCountry = {};
-var countryCodeMap = {};
+const _ = require("lodash");
+var teamsByCountry = {
+  // india: [],
+};
+var countryCodeMap = {
+  // indiaId: india
+};
 
-function getLeagueCountryData(countryRawData, leagueRawData) {
-  leagueByCountry = {};
+function getTeamCountryData(countryRawData, teamRawData) {
+  teamsByCountry = {};
   countryCodeMap = {};
-  for (i in countryRawData["data"]) {
-    countryCodeMap[countryRawData["data"][i]["id"]] =
-      countryRawData["data"][i]["name"];
+  for (let i in countryRawData) {
+    countryCodeMap[countryRawData[i]["id"]] = countryRawData[i]["name"];
   }
-  for (i in leagueRawData["data"]) {
-    country = countryCodeMap[leagueRawData["data"][i]["country_id"]];
-    league = leagueRawData["data"][i]["name"];
-    if (country in leagueByCountry) {
-      leagueByCountry[country].push(league);
+
+  for (i in teamRawData) {
+    let country = countryCodeMap[teamRawData[i]["country_id"]];
+    let team = teamRawData[i]["name"];
+    if (!country) continue;
+    if (country in teamsByCountry) {
+      teamsByCountry[country].push(team);
     } else {
-      leagueByCountry[country] = [league];
+      teamsByCountry[country] = [country];
     }
   }
 }
@@ -24,43 +30,62 @@ function getLeagueCountryData(countryRawData, leagueRawData) {
 const api_calls = async () => {
   var countriesAPI =
     CONFIG.PUBLISHER_DOMAIN + "countries?api_token=" + CONFIG.API_KEY;
-  var leaguesAPI =
-    CONFIG.PUBLISHER_DOMAIN + "leagues?api_token=" + CONFIG.API_KEY;
+  var teamsAPI = CONFIG.PUBLISHER_DOMAIN + "teams?api_token=" + CONFIG.API_KEY;
   var configCountries = {
     method: "get",
     url: countriesAPI,
     headers: {},
   };
-  var configLeagues = {
+  var configTeams = {
     method: "get",
-    url: leaguesAPI,
+    url: teamsAPI,
     headers: {},
   };
 
   try {
     var countryRawData = await axios(configCountries);
-    var leagueRawData = await axios(configLeagues);
-  } catch {}
-  if (countryRawData != undefined && leagueRawData != undefined) {
-    getLeagueCountryData(countryRawData.data, leagueRawData.data);
+    var teamRawData = await axios(configTeams);
+  } catch (err) {
+    console.log("Error", err);
+  }
+
+  if (!_.isEmpty(countryRawData) && !_.isEmpty(teamRawData)) {
+    getTeamCountryData(countryRawData.data?.data, teamRawData.data?.data);
   }
 };
 
 var io = require("socket.io")(3004);
-io.on("connection", function (socket) {
+
+io.on("connection", async (socket) => {
   console.log("connected:", socket.client.id);
   socket.on("serverEvent", function (data) {
     console.log("new message from client:", data);
   });
-  setInterval(function () {
-    api_calls();
-    console.log(leagueByCountry);
-    if (Object.keys(leagueByCountry).length > 0) {
-      let data_format = {
-        topicId: "d86e7021-b64e-442e-a905-fbca95d1544e",
-        topicData: leagueByCountry,
+
+  await api_calls();
+
+  let finalArr = [];
+
+  for (const [key, value] of Object.entries(countryCodeMap)) {
+    if (Object.keys(teamsByCountry).find((x) => x == value)) {
+      // callBroker(key, value, socket);
+      let topicData = {
+        countryId: key,
+        countryName: value,
+        countryTeams: teamsByCountry[value],
       };
-      socket.emit("publisher1", data_format);
+      let dataFormat = {
+        topicId: "d86e7021-b64e-442e-a905-fbca95d1544e",
+        topicData: topicData,
+      };
+      finalArr.push(dataFormat);
     }
-  }, 10000);
+  }
+
+  for (var i in finalArr) {
+    let local = i;
+    setTimeout(function () {
+      socket.emit("publisher1", finalArr[local]);
+    }, local * 10000);
+  }
 });
