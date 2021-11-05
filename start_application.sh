9fdad3233d1642285b198d44d7f0c8ed9baec396
Rahul Sharma <rahulsharma@Rahuls-Air-143.lan>
2021-11-05 18:10:47 -0400

    run

--- start_application.sh
+++ start_application.sh
@@ -1,18 +1,16 @@
 cd client
 docker build -t cricket-client .
-# cd ..
-# docker build -t cricket-api .
 cd ../Publisher1
 docker build -t cricket-api-publisher-1 .
+cd ../Publisher2
+docker build -t cricket-api-publisher-2 .
+cd ../Publisher3
+docker build -t cricket-api-publisher-3 .
 cd ../server1
 docker build -t broker-node-1 .
 cd ../server2
 docker build -t broker-node-2 .
 cd ../server3
 docker build -t broker-node-3 .
-# cd ../Publisher2
-# docker build -t cricket-api-publisher-2 .
-# cd ../Publisher3
-# docker build -t cricket-api-publisher-3 .
 cd ..
 docker-compose up --remove-orphans
